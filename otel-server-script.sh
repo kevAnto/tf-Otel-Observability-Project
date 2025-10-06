5727d961a80fc44a46849af789698cd6ebc355b2
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2025-10-06 12:08:27 -0400

    chore: storage added

--- otel-server-script.sh
+++ otel-server-script.sh
@@ -11,3 +11,15 @@ sudo su
 apt update
 apt install docker.io -y
 systemctl status docker
+
+# Ajouter au fstab pour montage automatique au démarrage
+echo '/dev/sdf /data ext4 defaults,nofail 0 2' | sudo tee -a /etc/fstab
+
+git clone https://github.com/open-telemetry/opentelemetry-demo.git
+cd opentelemetry-demo/
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker compose up --force-recreate --remove-orphans --detach
+
--- otel-server.tf
+++ otel-server.tf
@@ -21,6 +21,23 @@ resource "aws_instance" "otel-server" {
   associate_public_ip_address = true
   user_data                   = file("otel-server-script.sh")
   tags = {
-    Name = "${var.env_prefix}-Master"
+    Name = "${var.env_prefix}-app-server"
   }
+}
+
+resource "aws_ebs_volume" "otel-server-volume" {
+  availability_zone = var.avail_zone
+  size              = 20
+  type              = "gp3"
+  encrypted         = true
+  
+  tags = {
+    Name = "${var.env_prefix}-app-server-volume"
+  }
+}
+
+resource "aws_volume_attachment" "otel-server-volume-attachment" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.otel-server-volume.id
+  instance_id = aws_instance.otel-server.id
 }
